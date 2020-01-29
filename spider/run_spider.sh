496f65a4471762c72b033a288bb976b7a6b901d4
Administrator <zhangzhen@eyee.com>
2020-01-30 01:18:14 +0800

    add detail of spider

--- spider/run_spider.sh
+++
@@ -1,3 +0,0 @@
-cd /home/carry/Outbreak_BD/spider
-/home/carry/p36venv/bin/python data_spider.py > /home/carry/Outbreak_BD/spider/log/spider_log_$(date +\%Y-\%m-\%d).log 2>&1
-find /home/carry/Outbreak_BD/log  -mtime +5 -name "*.log"  -exec rm -rf {} \;
