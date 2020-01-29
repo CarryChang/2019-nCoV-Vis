b67d35e5c9ef196421f6b27f90fe8c8d85ea65ad
Administrator <zhangzhen@eyee.com>
2020-01-30 01:14:38 +0800

    add detail of spider

--- spider/dao/es_dao.py
+++ spider/dao/es_dao.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 def es_connect():
     import elasticsearch
-    # es = elasticsearch.Elasticsearch([{'host': '', 'port': 9200, "http_auth": ('elastic', 'Elastic1024')}])
-    es = elasticsearch.Elasticsearch([{'host': '175.24.69.6', 'port': 9200, "timeout": 30}])
+    es = elasticsearch.Elasticsearch([{'host': 'localhost', 'port': 9200, "http_auth": ('elastic', 'Elastic1024')}])
     return es
 # print(es_connect())
--- spider/run_spider_all.sh
+++
@@ -1,3 +0,0 @@
-cd /home/carry/Outbreak_BD/spider
-/home/carry/p36venv/bin/python data_spider_all.py > /home/carry/Outbreak_BD/spider/log/spider_all_log_$(date +\%Y-\%m-\%d).log 2>&1
-find /home/carry/Outbreak_BD/log  -mtime +5 -name "*.log"  -exec rm -rf {} \;
